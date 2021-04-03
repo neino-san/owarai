abf371ec815c288c73aaf21679e5d7367fd75074
neino-san <misesugreenapple@gmail.com>
2021-04-03 21:06:55 +0900

    コメントの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # includesメソッドを用いてn+1問題の解消
     @posts = @user.posts.page(params[:page]).per(10).order('updated_at DESC').includes(:user)
   end
 
